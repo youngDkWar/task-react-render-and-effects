1086d5ca1a1275b76f70408bc58660b2a760ca7b
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-05-23 13:20:51 +0500

    Completed

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,20 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+    const callback = (message: number) => setMessage(message);
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setMessage(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
